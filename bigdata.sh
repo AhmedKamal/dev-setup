96663a68b0784a242e94c78cefc820d9792b2a88
Ahmed Kamal <a.abdelfatah@live.com>
2019-03-03 17:24:30 +0400

    Get ride of python 2 installations

--- bigdata.sh
+++ bigdata.sh
@@ -15,56 +15,56 @@ echo "------------------------------"
 echo "Source virtualenvwrapper from ~/.extra"
 source ~/.extra
 
-###############################################################################
-# Python 2 Virtual Enviroment                                                 #
-###############################################################################
+# ###############################################################################
+# # Python 2 Virtual Enviroment                                                 #
+# ###############################################################################
 
-echo "------------------------------"
-echo "Updating py2-data virtual environment with AWS modules."
+# echo "------------------------------"
+# echo "Updating py2-data virtual environment with AWS modules."
 
-# Create a Python2 data environment
-# If this environment already exists from running pydata.sh,
-# it will not be overwritten
-mkvirtualenv py2-data
-workon py2-data
+# # Create a Python2 data environment
+# # If this environment already exists from running pydata.sh,
+# # it will not be overwritten
+# mkvirtualenv py2-data
+# workon py2-data
 
-pip install boto
-pip install awscli
-pip install s3cmd
+# pip install boto
+# pip install awscli
+# pip install s3cmd
 
-EXTRA_PATH=~/.extra
-echo $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "# Configure aws cli autocomplete, added by aws.sh" >> $EXTRA_PATH
-echo "complete -C '~/.virtualenvs/py2-data/bin/aws_completer' aws" >> $EXTRA_PATH
-source $EXTRA_PATH
+# EXTRA_PATH=~/.extra
+# echo $EXTRA_PATH
+# echo "" >> $EXTRA_PATH
+# echo "" >> $EXTRA_PATH
+# echo "# Configure aws cli autocomplete, added by aws.sh" >> $EXTRA_PATH
+# echo "complete -C '~/.virtualenvs/py2-data/bin/aws_completer' aws" >> $EXTRA_PATH
+# source $EXTRA_PATH
 
 ###############################################################################
 # Python 3 Virtual Enviroment                                                 #
 ###############################################################################
 
-#echo "------------------------------"
-#echo "Updating py3-data virtual environment with AWS modules."
+echo "------------------------------"
+echo "Updating py3-data virtual environment with AWS modules."
 
 # Create a Python3 data environment
 # If this environment already exists from running pydata.sh,
 # it will not be overwritten
-#mkvirtualenv --python=/usr/local/bin/python3 py3-data
-#workon py3-data
+mkvirtualenv --python=/usr/local/bin/python3 venv
+workon venv
 
-#pip install boto
-#pip install awscli
-#pip install s3cmd  # Python 2 only
+pip install boto
+pip install awscli
+pip install s3cmd  # Python 2 only
 
-# Uncomment if you want to hook up the aws cli autocomplete for Python 3
-#EXTRA_PATH=~/.extra
-#echo $EXTRA_PATH
-#echo "" >> $EXTRA_PATH
-#echo "" >> $EXTRA_PATH
-#echo "# Configure aws cli autocomplete, added by aws.sh" >> $EXTRA_PATH
-#echo "complete -C '~/.virtualenvs/py3-data/bin/aws_completer' aws" >> $EXTRA_PATH
-#source $EXTRA_PATH
+Uncomment if you want to hook up the aws cli autocomplete for Python 3
+EXTRA_PATH=~/.extra
+echo $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+echo "# Configure aws cli autocomplete, added by aws.sh" >> $EXTRA_PATH
+echo "complete -C '~/.virtualenvs/venv/bin/aws_completer' aws" >> $EXTRA_PATH
+source $EXTRA_PATH
 
 ###############################################################################
 # System-Wide Packages                                                        #
